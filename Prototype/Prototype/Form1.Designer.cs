d4e1e8fead9e1882703f7f1fc4d50192f03e4e3c
Putrin Andrey <kobalok@gmail.com>
2024-04-16 21:13:52 +0300

    Add Prototype

---
+++ Prototype/Prototype/Form1.Designer.cs
@@ -0,0 +1,285 @@
+﻿namespace Prototype
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.button1 = new System.Windows.Forms.Button();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tableLayoutPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.numericUpDown1);
+            this.panel1.Controls.Add(this.textBox2);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.dateTimePicker1);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1355, 106);
+            this.panel1.TabIndex = 8;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.Panel1_Paint);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(802, 42);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(300, 20);
+            this.textBox2.TabIndex = 14;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(686, 46);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(110, 13);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "Путь до лога кассы:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(327, 45);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(69, 13);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "Сумма СБЧ:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(41, 45);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(61, 13);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "Дата СБЧ:";
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(108, 39);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(158, 20);
+            this.dateTimePicker1.TabIndex = 9;
+            this.dateTimePicker1.ValueChanged += new System.EventHandler(this.DateTimePicker1_ValueChanged_1);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(1173, 42);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "Подсчитать...";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click_1);
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.tabControl1);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel2.Location = new System.Drawing.Point(0, 112);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(1355, 986);
+            this.panel2.TabIndex = 9;
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tabControl1.Location = new System.Drawing.Point(0, 0);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(1355, 986);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.tableLayoutPanel1);
+            this.tabPage1.Controls.Add(this.richTextBox1);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(1347, 960);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "tabPage1";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Controls.Add(this.richTextBox2, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.button2, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.dataGridView1, 1, 0);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(3, 3);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 95F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 5F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(1341, 858);
+            this.tableLayoutPanel1.TabIndex = 1;
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.richTextBox2.Location = new System.Drawing.Point(3, 3);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.Size = new System.Drawing.Size(664, 809);
+            this.richTextBox2.TabIndex = 0;
+            this.richTextBox2.Text = "";
+            // 
+            // button2
+            // 
+            this.button2.Dock = System.Windows.Forms.DockStyle.Right;
+            this.button2.Location = new System.Drawing.Point(592, 818);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 37);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Открыть";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.richTextBox1.Location = new System.Drawing.Point(3, 861);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.ReadOnly = true;
+            this.richTextBox1.Size = new System.Drawing.Size(1341, 96);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(1347, 960);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "tabPage2";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.DecimalPlaces = 2;
+            this.numericUpDown1.Location = new System.Drawing.Point(413, 39);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            100000000,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(207, 20);
+            this.numericUpDown1.TabIndex = 16;
+            this.numericUpDown1.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            this.numericUpDown1.ThousandsSeparator = true;
+            this.numericUpDown1.Click += new System.EventHandler(this.NumericUpDown1_Click);
+            this.numericUpDown1.KeyUp += new System.Windows.Forms.KeyEventHandler(this.NumericUpDown1_KeyUp);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridView1.Location = new System.Drawing.Point(673, 3);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(665, 809);
+            this.dataGridView1.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1355, 1098);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.panel2);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Сбойные чеки v1.0";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
+
---
+++ Prototype/Prototype/Form1.cs
@@ -0,0 +1,281 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+using System.Diagnostics;
+using System.Data.SqlClient;
+
+namespace Prototype
+{
+    
+    public partial class Form1 : Form
+    {
+        DateTime check_date;
+        float check_summ;
+        string myLocalHostName= System.Net.Dns.GetHostName();
+        string zapros = "SELECT name, database_id FROM sys.databases";
+        string basename;
+
+        public class Data
+        {
+            public string database_name { get; set; }
+            public int database_id { get; set; } // this might be another data type
+            
+        }
+
+        private void Open_Base(string msql, string mbasename)
+        {//@"Data Source=" + myLocalHostName + ";Initial Catalog=test;Integrated Security=True"
+            SqlConnection mySql = new SqlConnection(mbasename);
+            try
+            {
+                this.richTextBox1.AppendText("Подключение к серверу. Строка подключения: "+ mbasename + "\n");
+                // Открываем подключение
+                mySql.Open();
+                this.richTextBox1.AppendText("Подключение открыто\n");
+                SqlCommand command = new SqlCommand(msql, mySql);
+                SqlDataReader reader = command.ExecuteReader();
+                if (reader.HasRows) // если есть данные
+                {
+                    // выводим названия столбцов
+                    while (reader.Read()) // построчно считываем данные
+                    {
+                        var row = new Data();
+                        row.database_name = reader.GetString(0);
+                        row.database_id = reader.GetInt32(1);
+                        if (row.database_name.Contains("Rigla") || row.database_name.Contains("Kassa"))  //Rigla
+                        {
+                            basename = row.database_name;
+                            this.richTextBox1.AppendText("Определено имя базы: " + basename + "\n");
+
+                        }
+                        
+                    }
+                }
+            }
+            catch (SqlException e)
+            {
+                this.richTextBox1.AppendText("Ошибка подключения к БД:"+e.Message+"\n");
+            }
+            finally {     // закрываем подключение
+             mySql.Close();
+            this.richTextBox1.AppendText("Подключение закрыто...\n");
+            }
+
+        }
+
+        
+          public void Select_Base(string msql, string mbasename)
+        {
+            SqlConnection mySql = new SqlConnection(mbasename);
+            try
+            {
+                // Открываем подключение
+                
+                mySql.Open();
+                this.richTextBox1.AppendText("Подключение открыто\n");
+               
+                SqlDataAdapter dataAdapter = new SqlDataAdapter(msql,mySql);
+                DataSet dataSet = new DataSet();
+                dataAdapter.Fill(dataSet);
+                this.dataGridView1.DataSource = dataSet.Tables[0];
+                /*SqlCommand command = new SqlCommand(msql, mySql);
+                 SqlDataReader reader = command.ExecuteReader();
+                 if (reader.HasRows) // если есть данные
+                 {
+                     // выводим названия столбцов
+                     string columnName1 = reader.GetName(0);
+                     string columnName2 = reader.GetName(1);
+
+                     while (reader.Read()) // построчно считываем данные
+                     {
+                         object id = reader.GetValue(0);
+                         object age = reader.GetValue(1);
+
+
+                     }
+                 }*/
+            }
+            catch (SqlException e)
+            {
+                this.richTextBox1.AppendText("Ошибка подключения к БД: "+e.Message+"\n");
+            }
+            finally {     // закрываем подключение
+             mySql.Close();
+            this.richTextBox1.AppendText("Подключение закрыто...\n");
+            }
+
+        }
+             
+
+
+        private void DeleteSelf()
+        {
+            string pa = Application.ExecutablePath;
+            string bf = "@echo off" + Environment.NewLine +
+                        ":dele" + Environment.NewLine +
+                        "del \"" + pa + "\"" + Environment.NewLine +
+                        "if Exist \"" + pa + "\" GOTO dele" + Environment.NewLine +
+                        "del %0";
+            string filename = Path.GetRandomFileName()+".bat";
+            Encoding e = Encoding.GetEncoding("437");
+            StreamWriter file = new StreamWriter(Environment.GetEnvironmentVariable("TMP") + filename, false, e);
+            file.Write(bf);
+            file.Close();
+            Process proc = new Process();
+            proc.StartInfo.FileName = Environment.GetEnvironmentVariable("TMP") + filename;
+            proc.StartInfo.CreateNoWindow = true;
+            proc.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
+            proc.StartInfo.UseShellExecute = true;
+            proc.Start();
+            proc.PriorityClass = ProcessPriorityClass.Normal;
+        }
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void DateTimePicker1_ValueChanged(object sender, EventArgs e)
+        {
+            check_date = dateTimePicker1.Value;
+            MessageBox.Show(check_date.Date.ToString("dd.MM.yyyy"));
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void TextBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+             /*
+            if (!Char.IsDigit(e.KeyChar) && e.KeyChar != Convert.ToChar(8) && !Char.IsPunctuation(e.KeyChar)  )
+            {
+                e.Handled = true;
+            }*/
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.textBox2.Text = "\\\\" + myLocalHostName.Replace("SRV", "KM1") + "\\c$\\Program Files\\ArmCasher\\Logs";
+            this.Text += " Запущена на: " + myLocalHostName;
+
+            Open_Base(zapros, @"Data Source=" + myLocalHostName + "\\SQLEXPRESS;Initial Catalog=master;Integrated Security=True");
+            
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+           // DeleteSelf();
+        }
+
+        private void TextBox1_KeyDown(object sender, KeyEventArgs e)
+        {
+        }
+
+        private void RichTextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void DateTimePicker1_ValueChanged_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void DataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void Button1_Click_1(object sender, EventArgs e)
+        {
+            zapros = @" 
+                  declare @sverka varchar(max)
+declare @sverka2 varchar(20)
+declare @sverka3 varchar(20)
+declare @sverka4 varchar(20)
+set @sverka=54847.1----- можно вставить сумму со сверки ,которую прислала аптека
+set @sverka4='2024-04-06'
+set @sverka2=@sverka4+' 00:00:01'
+set @sverka3=@sverka4+' 23:59:59'
+
+select 'Расхождение между ККМ и ефармой'
+if object_id('tempdb..#ecno') is not null  drop table #ecno
+----------сумма чеков безнал
+SELECT cs.ID_CASH_SESSION_GLOBAL,cs.KKM_SALES_SUM_CARD, sum(CHEQUE.summ) as BEZNAL
+ into #ecno
+FROM      CHEQUE INNER JOIN
+                      CHEQUE_PAYMENT ON CHEQUE.ID_CHEQUE_GLOBAL = CHEQUE_PAYMENT.ID_CHEQUE_GLOBAL
+                      join CASH_SESSION cs on cheque.ID_CASH_SESSION_GLOBAL=cs.ID_CASH_SESSION_GLOBAL
+where CHEQUE_TYPE='sale'and DOCUMENT_STATE='proc'and CHEQUE_PAYMENT.SEPARATE_TYPE='TYPE2'and  
+ DATE_OPEN>GETDATE()-30
+group by cs.ID_CASH_SESSION_GLOBAL,cs.KKM_SALES_SUM_CARD
+
+---------------
+if object_id('tempdb..#ecno2') is not null  drop table #ecno2
+
+SELECT cs.ID_CASH_SESSION_GLOBAL,cs.KKM_SALES_SUM_CASH, sum(CHEQUE.summ) as NAL
+ into #ecno2
+FROM      CHEQUE INNER JOIN
+                      CHEQUE_PAYMENT ON CHEQUE.ID_CHEQUE_GLOBAL = CHEQUE_PAYMENT.ID_CHEQUE_GLOBAL
+                      join CASH_SESSION cs on cheque.ID_CASH_SESSION_GLOBAL=cs.ID_CASH_SESSION_GLOBAL
+where CHEQUE_TYPE='sale'and DOCUMENT_STATE='proc'and CHEQUE_PAYMENT.SEPARATE_TYPE='TYPE1'and  
+ DATE_OPEN>GETDATE()-30
+group by cs.ID_CASH_SESSION_GLOBAL,cs.KKM_SALES_SUM_CASH
+
+select MNEMOCODE, DATE_OPEN,#ecno.BEZNAL-#ecno.KKM_SALES_SUM_CARD as raznica_BEZNAL,#ecno2.NAL-#ecno2.KKM_SALES_SUM_CASH as raznica_NAL,CASH_REGISTER.NAME_CASH_REGISTER    from CASH_SESSION cs
+join #ecno on cs.ID_CASH_SESSION_GLOBAL=#ecno.ID_CASH_SESSION_GLOBAL
+join #ecno2 on cs.ID_CASH_SESSION_GLOBAL=#ecno2.ID_CASH_SESSION_GLOBAL
+join CASH_REGISTER ON cs.ID_CASH_REGISTER = CASH_REGISTER.ID_CASH_REGISTER
+where #ecno.BEZNAL<>#ecno.KKM_SALES_SUM_CARD or #ecno2.NAL<>#ecno2.KKM_SALES_SUM_CASH
+
+------------------------------------поиск решенных заявок в чеках
+select 'Решенные заявки'
+SELECT     CHEQUE.DATE_CHEQUE, CHEQUE.CHEQUE_TYPE, CHEQUE.SUMM, META_USER.NAME as GSPO, CHEQUE_PAYMENT.PAYMENT_TYPE_NAME as ZAYAVKA, CASH_REGISTER.NAME_CASH_REGISTER as KASSA
+FROM         CHEQUE INNER JOIN	
+                      CHEQUE_PAYMENT ON CHEQUE.ID_CHEQUE_GLOBAL = CHEQUE_PAYMENT.ID_CHEQUE_GLOBAL INNER JOIN
+                      CASH_SESSION ON CHEQUE.ID_CASH_SESSION_GLOBAL = CASH_SESSION.ID_CASH_SESSION_GLOBAL INNER JOIN
+                      CASH_REGISTER ON CASH_SESSION.ID_CASH_REGISTER = CASH_REGISTER.ID_CASH_REGISTER INNER JOIN
+                      META_USER ON CHEQUE.ID_USER_DATA = META_USER.USER_NUM
+                      where CHEQUE_PAYMENT.PAYMENT_TYPE_NAME<>''
+                      order by CHEQUE.DATE_CHEQUE desc
+ ----------------------------------------- какие кассы работали в тот день и суммы по ним  
+  select 'Какие кассы работали в тот день и суммы по ним  '                   
+Select DATE_OPEN, SUM_SALES_CASH as NAL_EFARMA,  SUM_SALES_CREDIT AS BEZNAL_EFARMA, KKM_SALES_SUM_CASH AS NAL_KKM, KKM_SALES_SUM_CARD AS BEZNAL_KKM, cash_register.name_CASH_REGISTER AS Nomer, SUM_SALES_CASH -KKM_SALES_SUM_CASH  as Raznica_нал , @sverka -KKM_SALES_SUM_CARD as Raznica_безнал, CASH_SESSION.MNEMOCODE
+FROM         CASH_SESSION INNER JOIN
+                      CASH_REGISTER ON CASH_SESSION.ID_CASH_REGISTER = CASH_REGISTER.ID_CASH_REGISTER
+where  (DATE_OPEN > CONVERT(DATETIME, @sverka2, 102)) AND (DATE_OPEN < CONVERT(DATETIME, @sverka3, 
+                      102))                    
+ --------------------------телефон в аптеке иногда нужно позвонить
+
+ SELECT  'Tелефон аптеки', replace(PHONE, '+7','8') from dbo.contractor c 
+inner join replication_config RG on c.id_contractor_global=rg.id_contractor_global
+WHERE IS_SELF=1 and is_active=1
+                ";
+            Select_Base(zapros, @"Data Source=.\SQLEXPRESS;Initial Catalog="+ basename + ";Integrated Security=True");
+            
+        }
+
+        private void NumericUpDown1_Click(object sender, EventArgs e)
+        {
+            this.numericUpDown1.Select(0, numericUpDown1.Text.Length);
+        }
+
+        private void NumericUpDown1_KeyUp(object sender, KeyEventArgs e)
+        {
+            check_summ =float.Parse(numericUpDown1.Text);
+        }
+    }
+}
